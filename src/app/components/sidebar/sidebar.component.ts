f23829d0cb6aee7f93fcd52a0acbe2e88da73c31
tejtesnim <109729700+tejtesnim@users.noreply.github.com>
2022-07-21 14:18:59 +0100

    proj

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+    { path: '/calendar', title: 'Calendar',  icon: 'calendar_today', class: '' },
+    { path: '/table-list', title: 'History',  icon:'content_paste', class: '' },  
+    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+    { path: '/user-profile', title: 'User Profile',  icon:'person', class: 'active-pro' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
---
+++ src/app/table-list/table-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-TableList',
+  templateUrl: './table-list.component.html',
+  styleUrls: ['./table-list.component.css']
+})
+export class TableListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
